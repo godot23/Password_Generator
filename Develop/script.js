a64ea802fd23c47c195eba554f2e02a1d99632fd
godot23 <jasonanavon@gmail.com>
2023-07-26 19:15:49 -0400

    made some changes, but there seems to be some kind of recusion error

--- Develop/script.js
+++ Develop/script.js
@@ -3,17 +3,49 @@ var generateBtn = document.querySelector("#generate");
 
 let lowerCaseAlphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 let upperCaseAlphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+let numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let symbols = ["!", "@", "#", "$", "%", "^", "&", "*", "/", "+", "-", "_", "=", "~"]
+let choices = [];
 
-// Write password to the #password input
+
 function generatePassword(){
-  if(confirm("lowercase allowed?")){
+  let password = "";
 
+  if(confirm("are lowercase letters allowed?")){
+    choices = [...choices, ...lowerCaseAlphabet];
   }
 
+  if(confirm("are uppercase letters allowed?")){
+    choices = [...choices, ...upperCaseAlphabet];
+  }
 
-// build string based off randomly selected values from an array 
+  if(confirm("are numbers allowed?")){
+    choices = [...choices, ...numbers];
+  }
 
-  return "your password here";
+  if(confirm("are special characters allowed?")){
+    choices = [...choices, ...symbols];
+  }
+ 
+  if(choices.length === 0){
+    emptySet();
+  }
+
+  let passwordLength = prompt("How long do you want the password");
+
+  for (let i = 0; i < passwordLength; i++){
+      password += choices[Math.floor(Math.random()*choices.length)];
+  }
+  choices = [];
+  return password;
+}
+
+function emptySet(){
+  let noItems = false;
+  while (noItems === false){
+    noItems = confirm("No items were selected. Please select OK to try again.");
+  }
+  generatePassword();
 }
 
 function writePassword() {
